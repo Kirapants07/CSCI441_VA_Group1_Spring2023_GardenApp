017935df82d6fc402a109524e4c3c1c22d61106f
Matt Williams <mwilliams@clinicalink.com>
2023-03-26 20:24:59 -0400

    adding base path for /user/index

---
+++ API/api/user/index.php
@@ -0,0 +1,14 @@
+<?php
+    //This serves as the zipcode controller
+
+    define('MyConst', TRUE);
+
+    //AUTH NEEDS TO GO HERE IF THE APP DOES NOT PROVIDE CREDENTIALS THEY GO NO FURTHER.
+
+    //END AUTH
+
+    include_once '../../model/users.php';
+    include_once '../../model/userData.php';
+    include_once '../../config/indexHeaderCRUD.php';
+
+?>
