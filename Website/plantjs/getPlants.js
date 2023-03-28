a2cb693cceff1d45a8c4a46ccbe1c7597c35081c
Todd <woodtm@comcast.net>
2023-03-27 22:17:54 -0700

    Plant Search, plus changes to index and App

---
+++ Website/plantjs/getPlants.js
@@ -0,0 +1,126 @@
+//getPlants
+//Todd M. Wood
+//Gets and Displays Plants
+
+import Plant from "./plantClass.js";
+
+
+const plantList = [];
+const getPlants = async (plantName) => {
+    console.log(plantName);
+    let jsonData = null;
+
+    if (!plantName){
+        const response = await fetch ("http://localhost/garden/CSCI441_VA_Group1_Spring2023_GardenApp/API/api/plant/");
+        jsonData = await response.json();
+    }
+    else{
+        let response = await fetch (`http://localhost/garden/CSCI441_VA_Group1_Spring2023_GardenApp/API/api/plant/?name=${plantName}`);
+
+        jsonData = await response.json();
+        if (jsonData.message){
+            response = await fetch (`http://localhost/garden/CSCI441_VA_Group1_Spring2023_GardenApp/API/api/plant/?pluralName=${plantName}`);
+            
+            jsonData = await response.json();
+            if (jsonData.message){
+            console.log(jsonData.message);
+            return false
+            }
+        }
+    }
+
+
+    for (let i = 0; i < jsonData.length; i++){
+        let temp = jsonData[i];
+        const newPlant = new Plant(temp.id,temp.name,temp.type,temp.spacing,temp.germinationInformation,temp.harvestInformation);
+        plantList.push(newPlant);   
+    }
+}
+
+const displayPlants = async (plantName) => {
+
+    await getPlants(plantName);
+    
+    let main = document.querySelector('main');
+    let element = (plantList) ? await createPlant(plantList) : 'There are no Plants to Display'; 
+    
+    main.append(element);
+    return element;
+
+}
+
+
+async function createPlant(plants)
+{
+    if (!plants) return;
+    let fragment = document.createDocumentFragment();
+    let table = document.createElement("table");
+    table.setAttribute("id", "pTable");
+    fragment.appendChild(table);
+
+    let title = document.createElement("tr");
+    table.appendChild(title);
+
+    let tName = document.createElement("th");
+    tName.textContent = "Name";
+    let tType = document.createElement("th");
+    tType.textContent = "Type";
+    let tSpacing = document.createElement("th");
+    tSpacing.textContent = "Spacing";
+    let tHarv = document.createElement("th");
+    tHarv.textContent = "Germination";
+    let tGerm = document.createElement("th");
+    tGerm.textContent = "Harvest";
+
+    title.appendChild(tName);
+    title.appendChild(tType);
+    title.appendChild(tSpacing);
+    title.appendChild(tGerm);
+    title.appendChild(tHarv);
+
+    for(let i=0; i < plants.length; i++)
+    {
+        let plant = plants[i];
+    
+        let newRow = document.createElement("tr"); 
+        table.appendChild(newRow);
+
+        let name = document.createElement("td");
+        name.textContent =  plant.getName();
+        let type = document.createElement("td");
+        type.textContent =  plant.getType();
+        let spacing = document.createElement("td");
+        spacing.textContent =  plant.getSpacing();
+        let germ = document.createElement("td");
+        germ.textContent =  plant.getGermination();
+        let harv = document.createElement("td");
+        harv.textContent =  plant.getHarvest();
+
+        newRow.appendChild(name);
+        newRow.appendChild(type);
+        newRow.appendChild(spacing);
+        newRow.appendChild(germ);
+        newRow.appendChild(harv);
+        
+    }
+    return fragment;
+}
+
+document.getElementById("pSearch").addEventListener("submit", (e) => {
+    e.preventDefault();
+    plantList.length = 0; // reset plantlist
+    const pTable = document.getElementById("pTable");
+    if(pTable)
+    {
+        pTable.parentNode.removeChild(pTable);
+    }
+    let inputName = document.getElementById("pName").value;
+    displayPlants(inputName);    
+}); 
+
+
+
+  
+
+
+
---
+++ Website/plantjs/plantClass.js
@@ -0,0 +1,35 @@
+//PlantClass
+//By Todd M. Wood
+
+export default class Plant {
+
+    constructor (plantId, plantName, plantType, plantSpacing, germination, harvest) {
+        this.id = plantId;
+        this.name = plantName;
+        this.type = plantType;
+        this.spacing = plantSpacing;
+        this.germ = germination;
+        this.harv = harvest;
+    }
+
+    getId(){
+        return this.id;
+    }
+    getName(){
+        return this.name;
+    }
+    getType(){
+        return this.type;
+    }
+    getSpacing(){
+        return this.spacing;
+    }
+    getGermination(){
+        return this.germ;
+    }
+    getHarvest(){
+        return this.harv;
+
+    }
+
+}
