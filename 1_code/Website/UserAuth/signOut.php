a40b203a717ea3ee7ad36ee032e26fbc167d8dd6
D-Dietrich <dadietrich@mail.fhsu.edu>
2023-04-28 18:06:47 -0500

    Modified signOut to fully wipe cookies.

--- 1_code/Website/UserAuth/signOut.php
+++ 1_code/Website/UserAuth/signOut.php
@@ -5,10 +5,12 @@ if(isset($_SESSION)){ //If there is an active session...
 
 if (isset($_COOKIE["username"])){
     unset($_COOKIE["username"]);
+    setcookie('username', null, -1, '/');
 }
 
 if (isset($_COOKIE["userID"])){
     unset($_COOKIE["userID"]);
+    setcookie('userID', null, -1, '/');
 }
 
 header ("Location: ../pages/login.html?sig=SO"); //Redirect to login page
