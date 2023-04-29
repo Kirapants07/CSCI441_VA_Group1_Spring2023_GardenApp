897803d677356d26fe9f034207bbceadaeab8ae5
Kira Luethe <ksluethe@mail.fhsu.edu>
2023-04-28 19:29:40 -0700

    navigation bar is fully responsive. Moved log in and sign up links to drop down menus

--- 1_code/Website/responsive.js
+++ 1_code/Website/responsive.js
@@ -1,10 +1,10 @@
 //Toggles between regular and mobile navbars
 
-export default function responsive() {
-    var x = document.querySelector("nav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function responsive() {
+  var x = document.querySelector("nav");
+  if (x.className === "") {
+    x.className += " responsive";
+  } else {
+    x.className = "";
   }
+}
