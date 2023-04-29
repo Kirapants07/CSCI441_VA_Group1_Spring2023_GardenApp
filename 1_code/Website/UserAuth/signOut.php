4821f5a7a76fa57d7c4cc39b62b3264d354bc1f7
D-Dietrich <dadietrich@mail.fhsu.edu>
2023-04-28 20:30:15 -0500

    Added PHPSESSID destruction to signOut.php.

--- 1_code/Website/UserAuth/signOut.php
+++ 1_code/Website/UserAuth/signOut.php
@@ -2,7 +2,7 @@
 if(isset($_SESSION)){ //If there is an active session...
     session_destroy(); //Destroy the active session
 }
-
+//Destroy all cookies
 if (isset($_COOKIE["username"])){
     unset($_COOKIE["username"]);
     setcookie('username', null, -1, '/');
@@ -11,6 +11,11 @@ if (isset($_COOKIE["username"])){
 if (isset($_COOKIE["userID"])){
     unset($_COOKIE["userID"]);
     setcookie('userID', null, -1, '/');
+}
+
+if (isset($_COOKIE["PHPSESSID"])){
+    unset($_COOKIE["PHPSESSID"]);
+    setcookie('PHPSESSID', null, -1, '/');
 }
 
 header ("Location: ../pages/login.html?sig=SO"); //Redirect to login page
